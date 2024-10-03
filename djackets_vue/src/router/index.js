0446a5691107554652b8091c2b62c0ec8b03ceda
HolbieWan <8841@holbertonstudents.com>
2024-10-03 17:13:28 +0200

    Add: view to display content of cart - functionnality to change contents of cart: Increment/Decrement content - Remove from cart

--- djackets_vue/src/router/index.js
+++ djackets_vue/src/router/index.js
@@ -3,6 +3,7 @@ import Home from '../views/Home.vue'
 import Product from '../views/Product.vue'
 import Category from '../views/Category.vue'
 import Search from '../views/Search.vue'
+import Cart from '../views/Cart.vue'
 
 const routes = [
   {
@@ -32,6 +33,11 @@ const routes = [
 	path: '/search',
 	name: 'search',
 	component: Search
+  },
+  {
+	path: '/cart',
+  	name: 'cart',
+  	component: Cart
   }
 ]
 
